a60a0bf43ddbc1a285dc8f68153debdadd25bca5
Magn3tism <chapagainsakshyam@gmail.com>
2023-01-22 12:21:30 +0545

    Add more statistics

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [avg, setAvg] = useState(0);
 
-  const handleClick = (stateVar, func) => {
+  const handleClick = (stateVar, func, value) => {
     func(stateVar + 1);
+    setAll(all + 1);
+    setAvg(avg + value);
   };
 
   return (
@@ -15,19 +19,19 @@ const App = () => {
       <h1>Give Feeeback</h1>
       <Button
         handleClick={() => {
-          handleClick(good, setGood);
+          handleClick(good, setGood, 1);
         }}
         text="Good"
       />
       <Button
         handleClick={() => {
-          handleClick(neutral, setNeutral);
+          handleClick(neutral, setNeutral, 0);
         }}
         text="Neutral"
       />
       <Button
         handleClick={() => {
-          handleClick(bad, setBad);
+          handleClick(bad, setBad, -1);
         }}
         text="Bad"
       />
@@ -36,14 +40,17 @@ const App = () => {
       <Feedback text="Good" value={good} />
       <Feedback text="Neutral" value={neutral} />
       <Feedback text="Bad" value={bad} />
+      <Feedback text="All" value={all} />
+      <Feedback text="Average" value={avg / all || 0} />
+      <Feedback text="Positive" value={(good / all) * 100 || 0} extra="%" />
     </div>
   );
 };
 
-const Feedback = ({ text, value }) => {
+const Feedback = ({ text, value, extra = "" }) => {
   return (
     <p>
-      {text}: {value}
+      {text}: {value} {extra}
     </p>
   );
 };
